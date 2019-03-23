117faa1867a5e9098a306b2455b33de9750cfb36
Ronbaba <qintong189450@gmail.com>
2019-03-23 10:29:26 +0800

    springboot

---
+++ subjectservice/src/main/java/com/subject/SubjectserviceApplication.java
@@ -0,0 +1,13 @@
+package com.subject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubjectserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubjectserviceApplication.class, args);
+    }
+
+}
